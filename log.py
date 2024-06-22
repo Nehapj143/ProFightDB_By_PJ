c9c18c023e38e3e08c849c674b0ea83a5bf47277
Nehapj143 <prahacherishedmoments@gmail.com>
2024-06-23 05:21:36 +0530

    Update log.py

--- log.py
+++ log.py
@@ -1,10 +1,10 @@
 import telebot
 
 # Initialize Telegram Bot
-bot = telebot.TeleBot('YOUR_TELEGRAM_BOT_TOKEN')
+bot = telebot.TeleBot('7198911495:AAHrRwSFiu8l6ONgDbUXiVCdCJxXPeHNYCc')
 
 # Log channel ID
-LOG_CHANNEL_ID = '@your_log_channel_username'  # Replace with your log channel username or ID
+LOG_CHANNEL_ID = '-1001329275814'  # Replace with your log channel username or ID
 
 # Function to log messages to the log channel
 def log_message(message):
